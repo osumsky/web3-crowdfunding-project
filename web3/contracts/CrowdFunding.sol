6dedac3fad420c63deef7777e361d77f04b027d0
osumsky <osumsky@gmail.com>
2023-02-16 23:58:46 +0200

    Added functions to Smart Contract

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -18,4 +18,50 @@ contract CrowdFunding {
     mapping(uint256 => Campaign) public campaigns;
 
     uint256 public numberOfCampaigns = 0;
+
+    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns(uint256) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+
+        // If deadline <= currentTimeStamp - throw error
+        require(campaign.deadline > block.timestamp , "The deadline should ba a date in the future");
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+
+        numberOfCampaigns++;
+
+        return numberOfCampaigns - 1; // Campaign ID
+    }
+
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
+        Campaign storage campaign = campaigns[_id];
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+        (bool sent, ) = payable(campaign.owner).call{value: amount}("");
+        if (sent) {
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
+
+    }
+
+    function getDonators(uint256 _id) view public returns (address[] memory, uint256[] memory){
+        return (campaigns[_id].donators, campaigns[_id].donations);
+    }
+
+    function getCampaigns() view public returns (Campaign[] memory){
+        Campaign[] memory allCampanigns = new Campaign[](numberOfCampaigns);
+        for (uint i = 0; i < numberOfCampaigns; i++) {
+            // Campaign storage item = campaigns[i];
+            // allCampanigns[i] = item;
+            allCampanigns[i] = campaigns[i];
+        }
+
+        return (allCampanigns);
+    }
 }
